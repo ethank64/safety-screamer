2dcf08b94887f43ae1b710b46bfe552e834a69f4
Ethan Knotts <eknotts64@gmail.com>
2024-11-23 18:32:55 -0500

    Initial Commit

---
+++ Safety Screamer/Safety_ScreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Safety_ScreamerApp.swift
+//  Safety Screamer
+//
+//  Created by Ethan Knotts on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Safety_ScreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Safety ScreamerTests/Safety_ScreamerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Safety_ScreamerTests.swift
+//  Safety ScreamerTests
+//
+//  Created by Ethan Knotts on 11/23/24.
+//
+
+import Testing
+@testable import Safety_Screamer
+
+struct Safety_ScreamerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
